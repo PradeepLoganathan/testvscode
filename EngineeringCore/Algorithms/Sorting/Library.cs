ee710ca9fdb07d4befe38ec50585c1882d97d0e7
Pradeep Loganathan <Pradeep Loganathan>
2016-11-10 12:34:30 +0530

    First Commit

---
+++ EngineeringCore/Algorithms/Sorting/Library.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace EngineeringCore.Algorithms.Sorting
+{
+    //Create extenstion methods for List types.duck typing and fluent 
+    //the first parameter this extends the  IList implementations 
+    public static class Bubblesort
+    {
+        
+        public static void BubblesortAscending<T>(this IList<T> collection, Comparer<T> comparer)
+        {
+            bool swapped;
+            do
+            {
+                swapped = false;
+                
+                for (int i =0; i < (collection.Count -1); i++)
+                {
+                    if( comparer.Compare(collection[i], collection[i+1]) > 0 )
+                    {
+                        var temp = collection[i];
+                        collection[i] = collection[i + 1];
+                        collection[i + 1] = temp;
+                        swapped = true;                        
+                    }
+                }
+
+            }while( swapped == true )  ;
+        }
+
+        public static void BubblesortDescending<T>(this IList<T> collection, Comparer<T> comparer)
+        {
+            if( comparer.Compare)
+                 
+        } 
+    }
+}
---
+++ tests/Tests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Xunit;
+using EngineeringCore.Algorithms.Sorting;
+using System.Collections.Generic;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [Fact]
+        public void Test1() 
+        {
+           List<int> numbers = new List<int> {9, 7, 6, 3, 2, 1};
+           List<int> numberstocompare = new List<int> {1, 2, 3, 6, 7, 9};
+
+            numbers.BubblesortAscending(Comparer<int>.Default);
+            Assert.True(Enumerable.SequenceEqual());
+            
+                     
+             
+        }
+    }
+}
